6428b94b470d0d1f90638836ec64ee0394d65268
Ruani Lazzarotto <67637806+ruanilazzarotto@users.noreply.github.com>
2022-12-12 00:31:27 -0300

    Create conditional.sh

---
+++ conditional.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+
+V1=""
+V2=""
+
+if [[ "$V1" = "$V2" ]]; then
+  echo "Same value"
+fi
+
+# alternative syntax
+if [[ "$V1" = "$V2" ]]
+then
+  echo "Same value"
+fi
+
+# alternative syntax 
+if test "$V1" = "$V2"
+then
+  echo "Same value"
+fi
+
+# alternative syntax
+if [ "$V1" = "$V2" ]
+then
+  echo "Same value"
+fi
+
+[ "$V1" = "$V2" ] && echo "Same value"
