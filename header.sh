b993453be194e2d3860bc5f542e6e722a7e65932
Ruani Lazzarotto <ruani.lazzarotto@voraztecnologia.com>
2022-12-14 21:59:27 -0300

    Create header.sh

---
+++ header.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+#
+# header.sh - Exemple of a script header
+#
+# Site:                     https://exemaple.com
+# Author:                   Ruani Lazzarotto
+# Maintenance:              Ruani Lazzarotto
+#
+# ------------------------------------------------------------------------ #
+#  Script details.
+#
+#  Usage exmaples:
+#      $ ./header.sh -d 1
+#      Execute the script on debug level 1.
+# ------------------------------------------------------------------------ #
