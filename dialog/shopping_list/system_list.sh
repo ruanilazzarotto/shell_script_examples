c3e0c6070d90dee0e88aefb9d454cd876706a205
Ruani Lazzarotto <ruani.lazzarotto@voraztecnologia.com>
2023-01-15 18:29:21 -0300

    imp input validations and cancel actions

--- dialog/shopping_list/system_list.sh
+++ dialog/shopping_list/system_list.sh
@@ -35,7 +35,7 @@ RED="\e[31;1m"
 # ------------------------------- FUNCTIONS ----------------------------------------- #
 ListItems () {
     # ignore comments or blank lines, send return to TEMP file
-    egrep -v "^#|^$" "$DB_FILE" | tr : ' ' > "$TEMP"
+    egrep -v "^#|^$" "$DB_FILE" | tr : ' '  > "$TEMP"  
     dialog --title "Items List" --textbox "$TEMP" 20 40
     rm -f "$TEMP"
 }
@@ -45,63 +45,72 @@ CheckItem () {
     grep -i -q -c "$1$SEP" "$DB_FILE"
 }
 
-GetItem () {
-    return $(eval dialog --stdout --menu \"Choose a item:\" 0 0 0 $1)
-}
-
-InsertItem () {
-    local last_id="$(egrep -v "^#|^$" $DB_FILE | sort -g | tail -n 1 | cut -d $SEP -f 1)"
-    local next_id="$(($last_id+1))"
-            
-    local name="$(dialog --title "New Item" --stdout --inputbox "Enter the item name" 0 0)"     
-    
-    CheckItem "$name" && {
-        dialog --title "Erro" --msgbox "Item already exists" 6 40
-        exit 1
-    }
-    
-    qty="$(dialog --title "New Item" --stdout --inputbox "Enter the item quantity" 0 0)"
-    
-    echo "$next_id$SEP$name$SEP$qty" >> "$DB_FILE"
-    dialog --title "Success" --msgbox "Item created successfully" 6 40
-    
-    ListItems 
-}
-
-RemoveItem () {
-    local items=$(egrep "^#|^$" -v "$DB_FILE" | sort -h | cut -d $SEP -f 1,2 | sed 's/:/ "/;s/$/"/') || "No items"
-    local item_id=$(eval dialog --stdout --menu \"Choose a item:\" 0 0 0 $items)
-           
-    grep -i -v "^$item_id$SEP" "$DB_FILE" > "$TEMP"
-    mv "$TEMP" "$DB_FILE"
-
-    dialog --msgbox "Item removed successfully" 0 0
-    ListItems
-
-}
 # ----------------------------------------------------------------------------------- #
 
 # ------------------------------- EXECUTION ----------------------------------------- #
 
 while :
 do
-    action=$(dialog --title "Items Management" \
+     action=$(dialog --title "Items Management 2.0" \
                     --stdout \
                     --menu "Choose an option:" \
                     0 0 0 \
                     list   "List all system items" \
                     insert "Insert a new item" \
+                    edit   "Edit item quantity"  \
                     remove "Remove a item" )
     [  $? -ne 0 ] && exit
     case $action in
         list) ListItems ;;
         insert) 
-            InsertItem
+            last_id="$(egrep -v "^#|^$" $DB_FILE | sort -g | tail -n 1 | cut -d $SEP -f 1)"
+            next_id="$(($last_id+1))"
+            
+            name="$(dialog --title "New Item" --stdout --inputbox "Enter the item name" 0 0)"
+            [ ! "$name" ] && continue
+            
+            CheckItem "$name" && {
+                dialog --title "Erro" --msgbox "Item already exists" 6 40
+                exit 1
+            }
+            
+            qty="$(dialog --title "New Item" --stdout --inputbox "Enter the item quantity" 0 0)"
+            [ $? -ne 0 ] && continue
+            
+            echo "$next_id$SEP$name$SEP$qty" >> "$DB_FILE"
+            dialog --title "Success" --msgbox "Item created successfully" 6 40
+            
+            ListItems 
+        ;;
+        edit)
+            items=$(egrep "^#|^$" -v "$DB_FILE" | sort -h | cut -d $SEP -f 1,2 | sed 's/:/ "/;s/$/"/')
+            item_id=$(eval dialog --stdout --menu \"Choose a item:\" 0 0 0 $items)
+            [ $? -ne 0 ] && continue
+            name=$(egrep "^$item_id$SEP" "$DB_FILE" | cut -d $SEP -f 2)
+
+            qty="$(dialog --title "Edit Item quantity" --stdout --inputbox "Enter the item quantity" 0 0)"
+            [ ! "$qty" ] && exit 1
+
+            grep -i -v "^$item_id$SEP" "$DB_FILE" > "$TEMP"
+            mv "$TEMP" "$DB_FILE"
+            echo "$next_id$SEP$name$SEP$qty" >> "$DB_FILE"
+
+            dialog --msgbox "Item updated successfully"
+            ListItems
         ;;
         remove)
-            RemoveItem
-        ;;
+            items=$(egrep "^#|^$" -v "$DB_FILE" | sort -h | cut -d $SEP -f 1,2 | sed 's/:/ "/;s/$/"/')
+            echo $items
+            item_id=$(eval dialog --stdout --menu \"Choose a item:\" 0 0 0 $items)
+            echo $item_id
+            [ $? -ne 0 ] && continue
 
+            grep -i -v "^$item_id$SEP" "$DB_FILE" > "$TEMP"
+            mv "$TEMP" "$DB_FILE"
+
+            dialog --msgbox "Item removed successfully"
+            ListItems
+        ;;
     esac
 done
 # ----------------------------------------------------------------------------------- #
